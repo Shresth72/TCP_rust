fe938916ab65a988765fb7b2d7f4277893114e73
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-05-05 14:56:14 +0530

    bash

--- run.sh
+++ run.sh
@@ -1,22 +1,28 @@
 #!/bin/bash 
 
+# cargo build
 cargo b --release
 
-# don't run if compilation fails
+# Exit status of last command exec
 ext=$?
+
+# Exit script on fail
 if [[ $ext -ne 0 ]]; then
     exit $ext
 fi
 
+# Assign subnet to tun0 process 
 sudo ip addr add 192.168.0.1/24 dev tun0
+# Activate Network Interface tun0
 sudo ip link set up dev tun0
+# Allow exec to perform network configuration tasks
 sudo setcap cap_net_admin=eip target/release/tcp_rust
 target/release/tcp_rust &
 
-# keeping the process id
+# Process ID of last bg process 
 pid=$!
 
+# Catch SIGINT (Ctrl C) or SIGTERM signals
 trap "kill $pid" INT TERM
-
 wait $pid
 
